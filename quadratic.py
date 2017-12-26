fdd2131f46de556359b994df4e84f6362bbbf95d
jimsybeldon <jimsybeldon@gmail.com>
2017-12-26 09:52:15 -0600

    Initial quadratic libary commit

---
+++ quadratic.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+import QuadLibrary as fuckyou
+
+
+a = float(input('a: '))
+b = float(input('b: '))
+c = float(input('c: '))
+result = fuckyou.QuadraticFunction(a, b, c)
+print(result)
+
+
+
