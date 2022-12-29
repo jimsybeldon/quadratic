6b45a05956a384c2f023b36b316ea9dfcfd91e89
JimSybeldon <JimPiSybeldon@yahoo.com>
2022-12-29 14:21:08 -0600

    does this work?

--- QuadLibrary.py
+++ QuadLibrary.py
@@ -4,8 +4,8 @@ import math
 
 def QuadraticFunction(a,b,c):
 
-	x1 = (-b + math.sqrt(pow(b,2) - 4*a*c))/(2*a)
-	x2 = (-b - math.sqrt(pow(b,2) - 4*a*c))/(2*a)
+	x1 = (-b + math.sqrt(pow(b,2)) - 4*a*c)/(2*a)
+	x2 = (-b - math.sqrt(pow(b,2)) - 4*a*c)/(2*a)
 
 	return [x1, x2]
 	
